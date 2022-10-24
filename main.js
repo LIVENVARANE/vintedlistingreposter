bba4e2d409416b418c6dffb6845d1b02bb271cea
LIVENVARANE <ulucqs@gmail.com>
2022-10-24 18:12:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function repost() {
+    var isDone = false;
+
+    var msg = document.getElementById('msg');
+    var token = document.getElementById('token');
+    var listing = document.getElementById('listing');
+
+    //started doing this and then realized i dont have any proxy debugger on this pc... aborting mission bruh
+
+    if(!isDone) {
+        msg.innerText = 'An error happened, please retry.';
+        msg.style.color = 'red';
+    }
+}
